54cbb4fce68923896b36fc9cc295e3a0cd5856a1
KauaneRibeir <dasilvaribeirokauane@gmail.com>
2024-06-26 21:41:22 -0300

    incompleto

---
+++ index.js
@@ -0,0 +1,14 @@
+const prompt = require("prompt-sync")({ sigint: true });
+let base = +prompt("Digite a BASE da operção")
+let expoente = +prompt("Digite o EXPOENTE da operção")
+let resultado = 0
+let baseauxiliar = base
+for (let i =  < expoente; i++) {
+    for (let j = < base; j++) {
+        resultado+=baseauxiliar
+    ;     
+    }
+    
+} 
+console.log(potecias)
+
